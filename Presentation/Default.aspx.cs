72262d62676ca28a7b09203bec037555a16fa1f8
Thabane <skinnybonafide@yahoo.com>
2015-05-14 12:22:29 +0200

    Update 3

--- Presentation/Default.aspx.cs
+++ Presentation/Default.aspx.cs
@@ -5,11 +5,52 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using DataTier;
+using Excel = Microsoft.Office.Interop.Excel;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        lblDisplay.Text = clsOther.TheString();
+        //lblDisplay.Text = clsOther.TheString();
+    }
+    protected void btnSimple_Click(object sender, EventArgs e)
+    {
+        Excel.Application xlApp = new Excel.Application();
+        Excel.Workbook xlBooks = xlApp.Workbooks.Add(1);
+        Excel.Worksheet xlSheet = (Excel.Worksheet)xlBooks.Sheets[1];
+
+        xlSheet.Cells[1, 3] = "Author Report";
+        xlSheet.Cells[3, 1] = "Author ID";
+        xlSheet.Cells[3, 2] = "First Name";
+        xlSheet.Cells[3, 3] = "Last Name";
+        xlSheet.Cells[3, 4] = "Phone";
+        xlSheet.Cells[3, 5] = "Address";
+
+        List<authors> getReport = clsFunctions.GetAllAuthors();
+        int counter = 5;
+        int TotalAuthors = 0;
+        foreach (authors item in getReport)
+        {
+            xlSheet.Cells[counter, 1] = item.au_id;
+            xlSheet.Cells[counter, 2] = item.au_lname;
+            xlSheet.Cells[counter, 3] = item.au_fname;
+            xlSheet.Cells[counter, 4] = item.phone;
+            xlSheet.Cells[counter, 5] = item.address;
+            counter++;
+            TotalAuthors++;
+
+        }
+        xlSheet.Cells[counter + 4, 3] = "Total Records is" + " " + counter;
+        xlApp.Visible = true;
+        clsFunctions.InsertEntry("Generated Simple Report");
+
+    }
+    protected void btnGrouped_Click(object sender, EventArgs e)
+    {
+        Excel.Application xlApp = new Excel.Application();
+        Excel.Workbook xlBooks = xlApp.Workbooks.Add(1);
+        Excel.Worksheet xlSheet = (Excel.Worksheet)xlBooks.Sheets[1];
+
+
     }
 }
--- Reports_LINQ/DataTier/clsFunctions.cs
+++ Reports_LINQ/DataTier/clsFunctions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DataTier
 {
@@ -17,7 +18,7 @@ namespace DataTier
         public static List<Emp_Details> EmployeeDetailsReport(string pub_id)
         {
             PubsEntities Context = new PubsEntities();
-            var Emp_data = from emp in Context.employee
+            var Emp_data = (from emp in Context.employee
                            join det in Context.jobs on emp.job_id equals det.job_id
                            join pub in Context.publishers on emp.pub_id equals pub.pub_id
                            where emp.pub_id == pub_id
@@ -27,7 +28,24 @@ namespace DataTier
                                LastName = emp.lname,
                                JobDescription = det.job_desc,
                                Publisher = pub.pub_name
-                           };
+                           }).ToList();
+            return Emp_data;
+        }
+        public static bool InsertEntry(string action)
+        {
+            string lastID = clsXmlConn.LogXML.Descendants("entry").Last().Element("id").Value;
+            int ID = Convert.ToInt16(lastID);
+            ID++;
+            var newLogEntry = new XElement("entry",
+                     new XElement("id", ID.ToString()),
+                     new XElement("action", action),
+                     new XElement("date", System.DateTime.Now.ToShortDateString()),
+                     new XElement("time", System.DateTime.Now.ToShortTimeString()));
+
+            clsXmlConn.LogXML.Element("Log").Add(newLogEntry);
+            clsXmlConn.LogXML.Save(clsXmlConn.LogXmlPath);
+
+            return true;
         }
 
         //Custom Object
@@ -37,6 +55,14 @@ namespace DataTier
             public string LastName { get; set; }
             public string JobDescription { get; set; }
             public string Publisher { get; set; }
+        }
+        //Xml Class Conn
+        public class clsXmlConn
+        {
+            //public static string LogXmlPath = System.Web.HttpContext.Current.Server.MapPath("Log.xml");
+            public static string LogXmlPath = System.AppDomain.CurrentDomain.BaseDirectory + "~/Log.xml";
+            public static XDocument LogXML = XDocument.Load(LogXmlPath);
+
         }
     }
 }
